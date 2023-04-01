c63db1508236cbf6809e4ea2ed9b8f7115e84f28
Yash <74345713+yash244466666@users.noreply.github.com>
2023-04-01 18:18:45 +0600

    Fix linters and solved the test problem

--- solver.rb
+++ solver.rb
@@ -13,11 +13,14 @@ class Solver
   end
 
   def self.fizzbuzz(number)
-    case number
-    when number % 15 then 'fizzbuzz'
-    when number % 3 then 'fizz'
-    when number % 5 then 'buzz'
-    else number.to_s
+    if (number % 3).zero? && (number % 5).zero?
+      'fizzbuzz'
+    elsif (number % 3).zero?
+      'fizz'
+    elsif (number % 5).zero?
+      'buzz'
+    else
+      number.to_s
     end
   end
 end
